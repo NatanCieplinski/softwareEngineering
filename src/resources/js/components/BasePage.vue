42654383817ac4cb5ed3948bd8180888189ced78
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2020-01-30 17:39:34 +0100

    Initial frontend tests

---
+++ src/resources/js/components/BasePage.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Prova</h2>
+</template>
---
+++ src/resources/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="class navbar-expand-sm navbar-dark mb-2" style="background-color: #822626">
+        <div class="container">
+            <a href="#" class="navbar-brand"><img src="asset('img/logo.gif')"></a>
+        </div>
+    </nav>
+</template>
