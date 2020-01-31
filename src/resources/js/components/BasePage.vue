ce65f810d5f8ae3826e2fea5f85f219a80a17459
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2020-01-31 13:28:20 +0100

    Navbar added

--- src/resources/js/components/BasePage.vue
+++ src/resources/js/components/BasePage.vue
@@ -1,3 +1,50 @@
 <template>
-    <h2>Prova</h2>
+    
 </template>
+<script>
+export default {
+    mounted(){
+        console.log("component mounted")
+    },
+    data() {
+        return {
+            data: [],
+            data: {
+                id: '',
+                nome: '',
+                username: '',
+                email: '',
+                email_verified_at: '',
+                is_bannato: '',
+                tipo_utente: '',
+                created_at: '',
+                updated_at: ''
+            },
+            first_page_url: '',
+            from: '',
+            last_page: '',
+            last_page_url: '',
+            next_page_url: '',
+            path: '',
+            per_page: '',
+            prev_page_url: '',
+            to: '',
+            total: ''
+        }
+    },
+    created(){
+        this.fetchUser()
+    },
+    methods: {
+        fetchUser(){
+            axios
+            .get('/api/utenti')
+            .then(response => (this.info = response))
+            .then(response => {
+                console.log();
+            })
+        }
+    }
+    
+}
+</script>
